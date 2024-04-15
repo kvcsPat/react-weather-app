35e724083afc40d098cabffd8b1b8014fb4f18d0
kvcsPat <smth.patrik@gmail.com>
2024-04-15 12:35:59 +0200

    created search bar

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,15 @@
+import Search from "./Components/Search/Search";
+import "./Style/reset.css";
+import "./Style/global.css";
+
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
---
+++ weather-app/src/Components/Search/Search.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api.js";
+
+export default function Search({ onSearchChange }) {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?&namePrefix=${inputValue}`,
+        geoApiOptions
+      );
+      const result = await response.json();
+      return {
+        options: result.data.map((city) => {
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name}`,
+          };
+        }),
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      isClearable
+      debounceTimeout={1300}
+      placeholder="Search for city"
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+}
